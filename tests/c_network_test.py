14d03dadfa8a99b21e44558c6ee8d2954ba868fb
Leon Jungemeyer <leon.jungemeyer@me.com>
2021-06-30 22:06:40 +0200

    Add bias to test cases

--- tests/c_network_test.py
+++ tests/c_network_test.py
@@ -99,6 +99,15 @@ class CNetworkTest(unittest.TestCase):
 
         self.assertEqual(net.forward([1, 1]), [1, 0.1, 1, 0])
 
+        edgeRight.active = True
+        nodeMiddle.bias = 1
+
+        self.assertEqual(net.forward([1, 1]), [1, 2.1, 2, 0])
+
+        nodeMiddle.active = False
+
+        self.assertEqual(net.forward([1, 1]), [1, 0.1, 0, 0])
+
 
 if "__main__" == __name__:
     unittest.main()
